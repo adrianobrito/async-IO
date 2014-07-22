6a67315c943a600dece506be1c833feddfea0719
adrianobrito <adrianosdebrito@gmail.com>
2014-07-22 09:26:23 -0300

    Changes to the Random Generator functionality.

--- src/servlet/AsyncServlet.java
+++ src/servlet/AsyncServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Callable;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.atmosphere.config.service.MeteorService;
 import org.atmosphere.cpr.AtmosphereResource.TRANSPORT;
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterFactory;
 import org.atmosphere.cpr.Meteor;
 import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor;
 
@@ -21,19 +25,28 @@ import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor;
 public class AsyncServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-
+	private static int i = 0;
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws IOException {
 		
+		Broadcaster broadcaster = BroadcasterFactory.getDefault().lookup("name");
+		if(broadcaster == null) broadcaster = BroadcasterFactory.getDefault().get("name");
+		
 		// Getting meteor service
-		Meteor meteor = (Meteor)req.getSession().getAttribute("meteor"); 
-		if(meteor == null)	meteor = Meteor.build(req);
+		Meteor meteor = Meteor.build(req);
+		meteor.setBroadcaster(broadcaster);
 		
 		// If it is Long Polling mode, sets the Persistent Connection
 		meteor.resumeOnBroadcast(meteor.transport() == TRANSPORT.LONG_POLLING ? true : false).suspend(-1);
 		
-		// Broadcasts a message with random number in a 2 seconds interval
-		meteor.schedule("Random Number " + Math.random(), 2);
+		// Broadcasts a message with random number in a 2 seconds interval;
+		meteor.schedule(new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				return "Random Number " + (i++);
+			}
+		}, 2);
 		
 	}
 		
